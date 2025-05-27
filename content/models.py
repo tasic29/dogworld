f82ec011eceaafca1e43dabf8c96df2ef21d9213
Milos Tasic <tasic29@gmail.com>
2025-05-27 12:44:29 +0200

    added blog/comment/rating models

--- content/models.py
+++ content/models.py
@@ -2,6 +2,20 @@ from django.db import models
 from django.conf import settings
 
 
+class Blog(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    image = models.ImageField(upload_to='blog_images/', blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts'
@@ -21,3 +35,30 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    blog = models.ForeignKey(
+        Blog, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField(max_length=1000)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.user.username} on {self.blog.title}'
+
+
+class Rating(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    blog = models.ForeignKey(
+        Blog, on_delete=models.CASCADE, related_name='ratings')
+    score = models.PositiveSmallIntegerField()
+
+    class Meta:
+        # Each user can rate a blog only once
+        unique_together = ['user', 'blog']
+
+    def __str__(self):
+        return f'{self.user.username} rated {self.blog.title} - {self.score} stars'
