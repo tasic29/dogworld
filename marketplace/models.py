a116887a2e2fde9df227a995a51972a0545b010b
Milos Tasic <tasic29@gmail.com>
2025-07-01 11:16:27 +0200

    created marketplace app models

--- marketplace/models.py
+++ marketplace/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from autoslug import AutoSlugField
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    slug = AutoSlugField(populate_from='title', unique=True)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(
+        max_digits=6, decimal_places=2, validators=[MinValueValidator(1)])
+    affiliate_url = models.URLField()
+    image = models.ImageField(
+        upload_to='product_images/', blank=True, null=True)
+    category = models.ForeignKey(
+        'Category', on_delete=models.SET_NULL, null=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-updated_at']
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = AutoSlugField(populate_from='name', unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
