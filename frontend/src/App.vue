97085b0a31c510d1c5dc62ae5305125a9acda5f9
Milos Tasic <tasic29@gmail.com>
2025-07-14 14:36:45 +0200

    created simple navbar component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,13 @@
+<template>
+  <div>
+    <Navbar />
+    <RouterView />
+  </div>
+</template>
+
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
-import HomeView from "./views/HomeView.vue";
+import Navbar from "./components/Navbar.vue";
 </script>
-
-<template>
-  <HomeView />
-</template>
 
 <style scoped></style>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<!-- src/components/Navbar.vue -->
+<template>
+  <nav class="bg-white shadow-md p-4 flex justify-between items-center">
+    <router-link to="/" class="text-xl font-bold text-blue-600"
+      >Dog World</router-link
+    >
+
+    <div class="space-x-4">
+      <router-link to="/" class="text-gray-700 hover:text-blue-600"
+        >Home</router-link
+      >
+      <router-link to="/blogs" class="text-gray-700 hover:text-blue-600"
+        >Blogs</router-link
+      >
+      <router-link to="/posts" class="text-gray-700 hover:text-blue-600"
+        >Posts</router-link
+      >
+      <router-link to="/marketplace" class="text-gray-700 hover:text-blue-600"
+        >Shop</router-link
+      >
+      <router-link to="/login" class="text-gray-700 hover:text-blue-600"
+        >Login</router-link
+      >
+    </div>
+  </nav>
+</template>
+
+<script setup></script>
